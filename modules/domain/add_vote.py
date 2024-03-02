47d4415fb950b6be5e68af058192595149ef142a
v <t.shagin@gmail.com>
2024-03-02 23:33:17 +0300

    feat: Доработан класс Vote

---
+++ modules/domain/add_vote.py
@@ -0,0 +1,37 @@
+"""Занимается процессом голосования за добавление новых участников"""
+
+
+class Vote:
+    """
+    Контролирует процесс голосования
+    """
+    def __init__(self, text, message_id, votes_to_pass):
+        self.votes_for = 0
+        self.votes_against = 0
+        self.text = text
+        self.message_id = message_id
+        self.votes_need_to_pass = votes_to_pass
+
+    def vote_for(self):
+        """
+        Добавляет 1 голос за
+        :return:
+        """
+        self.votes_for += 1
+        return self.check_votes()
+
+    def vote_against(self):
+        """
+        Добавляет 1 голос против
+        :return:
+        """
+        self.votes_against += 1
+
+    def check_votes(self):
+        """
+        Проверяет не проголосовало ли за больше половины группы
+        :return:
+        """
+        if self.votes_for >= self.votes_need_to_pass:
+            return True
+        return False
